6799e2e747c0e4056a84e51e8a91f76e777e9641
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-03-06 01:21:20 +0530

    taoyao: Include Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    * Sony and Motorola Dolby Atoms both support this adaption
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Change-Id: I99240aed6b3ad6ac16506f755512bf13c3f939bd
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,11 +64,11 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 # HIDL
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
 
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/hidl/manifest_lahaina.xml \
     $(DEVICE_PATH)/hidl/manifest_taoyao.xml \
     $(DEVICE_PATH)/hidl/manifest_xiaomi.xml
