3a8af71fb1c15b963052635674c1ccc4f3dcd261
Silesh K Nair <sileshn@gmail.com>
2025-12-16 04:36:06 +0100

    taoyao: Shim _ZN4Json5ValueC1Ex for libmisight
    This fixes the following compilation error:
    
    vendor/xiaomi/sm8350-common/proprietary/vendor/lib64/libmisight.so: error: Unresolved symbol: _ZN4Json5ValueC1Ex
    
    [Tashar02: Fix copyright header style, reword commit message and description]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -421,6 +421,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
     sensors.dynamic_sensor_hal
 
+# Shim
+PRODUCT_PACKAGES += \
+    libmisightjson_shim
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
--- extract-files.py
+++ extract-files.py
@@ -81,7 +81,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/android.hardware.secure_element@1.0-impl.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
     'vendor/lib64/libmisight.so': blob_fixup()
-        .add_needed('libjsoncpp_shim.so'),
+        .add_needed('libjsoncpp_shim.so')
+        .add_needed('libmisightjson_shim.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
          .add_needed('libcrypto_shim.so'),
     ('vendor/lib/libstagefright_soft_ac4dec.so', 'vendor/lib/libstagefright_soft_ddpdec.so' 'vendor/lib/libstagefrightdolby.so', 'vendor/lib64/libdlbdsservice.so', 'vendor/lib64/libstagefright_soft_ac4dec.so', 'vendor/lib64/libstagefright_soft_ddpdec.so', 'vendor/lib64/libstagefrightdolby.so'): blob_fixup()
