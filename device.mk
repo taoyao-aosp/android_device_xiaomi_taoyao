1c37e6449fdcae60460428f9ceedd56f799b67d9
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-02-01 19:50:41 +0530

    taoyao: Build missing libcamera_metadata.vendor This is needed by mialgoengine.so

--- device.mk
+++ device.mk
@@ -120,6 +120,7 @@ PRODUCT_PACKAGES += \
     libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera_metadata.vendor \
     android.hardware.graphics.common-V4-ndk \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
