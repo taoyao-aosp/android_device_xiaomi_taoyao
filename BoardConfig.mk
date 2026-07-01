8879f892247d38cb51e45d837d6e313fa1483394
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:32:24 -0400

    taoyao: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,8 +167,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
-# Sepolicy
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+# SELinux
+include device/lineage/sepolicy/libion/sepolicy.mk
+include device/custom/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
