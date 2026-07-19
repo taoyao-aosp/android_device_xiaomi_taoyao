45cb04d6e918079600620049361e2ff34deeadf5
nullptr03 <nullptr03@singkolab.my.id>
2026-07-19 11:40:29 +0800

    taoyao: Move to power hal ext to handle dt2w
    Handle dt2w using xiaomi-touch since we have 2 different touch drivers.

--- device.mk
+++ device.mk
@@ -396,6 +396,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-taoyao)
+
 # PowerShare
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
 
