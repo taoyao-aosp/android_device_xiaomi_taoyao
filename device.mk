e41ca63527eae3483ba671522ca09f2f6bcae427
johnmart19 <johnivan19999@gmail.com>
2026-07-23 05:30:51 +0300

    taoyao: Build libperfmgr for Lineage Power Service
    I init    : starting service 'vendor.power-hal-aidl'...
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.power-service.lineage-libperfmgr": library "libperfmgr.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -389,7 +389,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
+    libqti-perfd-client \
+    libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
