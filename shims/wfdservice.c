4915bff92f28eac1391a22e96173ca45fe6ac0e3
skenakun <ajansyidad2014@gmail.com>
2026-08-14 04:16:49 +0000

    taoyao: Dropped WFD shims

--- shims/wfdservice.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2023 The LineageOS Project
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-void _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t() {
-    return;
-}
